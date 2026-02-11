9f0770d7abcea7a391382fc6179e902ab8888cd9
Stan-21 <shung951@gmail.com>
2026-02-11 14:54:06 -0800

    finished for midterm

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Godot Button Theme Generator
+### Godot Button Theme Generator
 
+# What the tool generates
+- This tool generates button themes for Godot.
+
+# How to run the tool
+- To run the tool, play main.tscn and click on the button that says "This Button Changes Those Buttons!"
+- Mess around with the sliders and toggles to influence the generation towards different types of buttons.
+- If you find a button that you like, click on it.  That will change the themes of the buttons on the right.
+- If you like it, click "Save button theme" and check the themes folder.  
+- Now you have a button theme that you can drag into any other Button node in any other Godot project!
+
+# Parameter Explanations
+- Warmness / Coolness: Influences the color of the button by making it closer to red-ish (warm) & blue-ish (cool)
+- Sharpness / Smoothness: Influences the border generation to make the edges more rounded or straight lines
+- Skew toggle: Toggles whether the buttons are skewed (slanted) or not.  If on, buttons are skewed by a range of 0-5
+
+# Example Outputs
+- Check example_output folder
+
+# Known Limitations
+- This generator is unable to generate buttons with gradients or patterns.
+- The button names that are generated aren't great.  They can sometimes conflict and override each other and will sometimes get the wrong color.
+- As of now, this generator is limited to only being run in Godot.  I would've liked to have it on itch.io, but I couldn't figure out downloading files..
--- main.gd
+++ main.gd
@@ -3,15 +3,52 @@ extends Node2D
 @onready var samples: Node = $Samples
 @onready var camera_2d: Camera2D = $Camera2D
 
-var warmness : float = 1.0
-var coolness : float = 1.0
+@onready var generate_button: Button = $CanvasLayer/VBoxContainer/GenerateButton
+@onready var warm: HSlider = $CanvasLayer/VBoxContainer/HBoxContainer/Warm
+@onready var cool: HSlider = $CanvasLayer/VBoxContainer/HBoxContainer2/Cool
+@onready var sharp: HSlider = $CanvasLayer/VBoxContainer/HBoxContainer4/Sharp
+@onready var smooth: HSlider = $CanvasLayer/VBoxContainer/HBoxContainer5/Smooth
+@onready var save_button: Button = $CanvasLayer/VBoxContainer/SaveButton
+
+
+var warmness : float = 0.5
+var coolness : float = 0.5
+var sharpness : float = 0.5
+var smoothness : float = 0.5
 
 var skewed : bool = true
+var drag : bool = false
+var offset : Vector2
+
 
 func _ready() -> void:
-	for i in samples.get_children():
+	for i : Button in samples.get_children():
+		i.pressed.connect(_on_button_clicked)
 		create_theme(i)
 
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			if event.is_pressed():
+				offset = get_viewport().get_mouse_position() - Vector2(1152.0 / 2, 648.0 / 2)
+				drag = true
+			if event.is_released():
+				drag = false
+				offset = Vector2.ZERO
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			if event.is_pressed(): # Check event.is_pressed() to confirm the event
+				camera_2d.zoom += Vector2(0.1, 0.1)
+				camera_2d.zoom = camera_2d.zoom.clamp(camera_2d.zoom, Vector2(5, 5))
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			if event.is_pressed():
+				camera_2d.zoom -= Vector2(0.1, 0.1)
+				camera_2d.zoom = camera_2d.zoom.clamp(Vector2(1, 1), camera_2d.zoom)
+
+func _physics_process(_delta: float) -> void:
+	if (drag):
+		camera_2d.position = get_viewport().get_mouse_position() - Vector2(1152.0 / 2, 648.0 / 2) - offset
+
+# HELPER FUNCTIONS FOR GENERATING THEMES
 func get_random_color() -> Color:
 	var color : Color = Color(randf(), randf(), randf(), 1)
 	
@@ -39,7 +76,6 @@ func set_border_width(style : StyleBoxFlat) -> void:
 	if (random == 1):
 		style.border_width_top = 5 # Border should be smaller in comparison
 		style.border_width_left = 5 # Need to add case for top right, bottom left, etc.
-		
 		style.border_color = style.bg_color - Color(0.2, 0.2, 0.2, 0.0)
 	if (random == 2):
 		style.border_width_bottom = 10
@@ -47,32 +83,56 @@ func set_border_width(style : StyleBoxFlat) -> void:
 	return
 	
 func set_corner_radius(style : StyleBoxFlat) -> void:
-	var random = randi_range(0, 2)
-	if (random == 0):
-		style.set_corner_radius_all(15)
-	if (random == 1):
-		style.corner_radius_top_left = 15 # Need to add case for bottom left / top right etc.
-		style.corner_radius_bottom_right = 15
+	if (randf() > sharpness):
+		var random = randi_range(0, 2)
+		if (random == 0):
+			style.set_corner_radius_all(15)
+		if (random == 1):
+			style.corner_radius_top_left = 15
+			style.corner_radius_bottom_right = 15
+		if (random == 2):
+			style.corner_radius_bottom_left = 15
+			style.corner_radius_top_right = 15
+		
 	return
 
 func set_shadow(style : StyleBoxFlat) -> void:
+	if (randf() < 0.3):
+		style.shadow_size = 0
+		return
 	style.shadow_size = randi_range(0, 20)
 	style.shadow_offset.y = randi_range(0, 5)
 	return
 
-func _on_button_2_pressed() -> void:
-	'''button.add_theme_color_override("font_color", get_random_color()) # Changes font color
-	button.add_theme_color_override("font_disabled_color", get_random_color()) # Changes when Disabled is toggled
-	button.add_theme_color_override("font_focus_color", get_random_color()) # Focus is when the button has been clicked down on 
-	button.add_theme_color_override("font_hover_color", get_random_color()) # When button is hovered over, self-explanatory
-	button.add_theme_color_override("font_hover_pressed_color", get_random_color()) # Uh in the name
-	button.add_theme_color_override("font_outline_color", get_random_color()) # Font outline idk what else to say lol, uhhhh I don't see it
-	
-	button.add_theme_color_override("icon_normal_color", get_random_color()) # Changes icon color, we are not dealing with icons'''
+func set_text_color(style : StyleBoxFlat, theme : Theme) -> void:
+	# If the color is 'bright' set the text color to black
+	if ((style.bg_color[0] + (style.bg_color[1] * 0.71) + style.bg_color[2]) / 3) > 0.5:
+		theme.set_color("font_color", "Button", Color(0, 0, 0))
+		theme.set_color("font_hover_color", "Button", Color(0, 0, 0))
+		theme.set_color("font_hover_pressed_color", "Button", Color(0, 0, 0))
 
-	for i in samples.get_children():
-		create_theme(i)
+func create_hover_theme(style : StyleBoxFlat) -> StyleBoxFlat:
+	var hover_style : StyleBoxFlat = style.duplicate()
 	
+	hover_style.bg_color = style.bg_color + Color(0.1, 0.1, 0.1, 0.0)
+	return hover_style
+	
+func create_pressed_theme(style : StyleBoxFlat) -> StyleBoxFlat:
+	var pressed_style : StyleBoxFlat = style.duplicate()
+	
+	if (pressed_style.get_border_width_min() > 0):
+		return pressed_style
+	
+	pressed_style.shadow_size = 0
+	pressed_style.border_blend = false
+	pressed_style.border_color = Color(0)
+	
+	pressed_style.border_width_top = style.border_width_bottom
+	pressed_style.border_width_bottom = style.border_width_top
+	pressed_style.border_width_left = style.border_width_right
+	pressed_style.border_width_right = style.border_width_left
+	return pressed_style
+		
 func create_theme(button : Button) -> void:
 	var theme : Theme = Theme.new()
 	
@@ -84,60 +144,112 @@ func create_theme(button : Button) -> void:
 	set_border_width(normal_style)
 	set_corner_radius(normal_style)
 	set_shadow(normal_style)
+	set_text_color(normal_style, theme)
 	#normal_style.set_corner_radius_all(randi_range(border_min, border_max))
-	normal_style.set_content_margin_all(10)
+	normal_style.set_content_margin_all(-1)
+	normal_style.set_expand_margin_all(5)
 	
 	theme.set_stylebox("normal", "Button", normal_style)
-	theme.set_stylebox("hover", "Button", normal_style)
-	theme.set_stylebox("pressed", "Button", normal_style)
+	theme.set_stylebox("hover", "Button", create_hover_theme(normal_style))
+	theme.set_stylebox("pressed", "Button", create_pressed_theme(normal_style))
 	button.theme = theme
+	
+# HELPER FUNCTION FOR UPDATING VALUES BASED ON SLIDERS
+func update_values() -> void:
+	warmness = warm.value
+	coolness = cool.value
+	sharpness = sharp.value
+	smoothness = smooth.value
 
+
+func _on_button_clicked() -> void:
+	for i in samples.get_children():
+		if (i.has_focus()):
+			generate_button.theme = i.theme
+			save_button.theme = i.theme
+		
+func _on_generate_pressed() -> void:
+	for i in samples.get_children():
+		create_theme(i)
+
+# When export button is pressed
 func _on_button_pressed() -> void:
 	for i in samples.get_children():
 		if (i.has_focus()):
-			var _resource = ResourceSaver.save(i.theme, "res://themes/themes.theme")
+			var _resource = ResourceSaver.save(i.theme, 
+			"res://themes/" + generate_button_name(i.theme) + "Bttn.theme")
 
-var drag : bool = false
-var offset : Vector2
-
-func _physics_process(_delta: float) -> void:
-	if (drag):
-		camera_2d.position = get_viewport().get_mouse_position() - Vector2(1152.0 / 2, 648.0 / 2) - offset
-
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_MIDDLE:
-			if event.is_pressed():
-				offset = get_viewport().get_mouse_position() - Vector2(1152.0 / 2, 648.0 / 2)
-				drag = true
-			if event.is_released():
-				drag = false
-				offset = Vector2.ZERO
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
-			if event.is_pressed(): # Check event.is_pressed() to confirm the event
-				camera_2d.zoom += Vector2(0.1, 0.1)
-				camera_2d.zoom = camera_2d.zoom.clamp(camera_2d.zoom, Vector2(5, 5))
-		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
-			if event.is_pressed():
-				camera_2d.zoom -= Vector2(0.1, 0.1)
-				camera_2d.zoom = camera_2d.zoom.clamp(Vector2(1, 1), camera_2d.zoom)
-
-@onready var warm: HSlider = $CanvasLayer/VBoxContainer/HBoxContainer/Warm
-@onready var cool: HSlider = $CanvasLayer/VBoxContainer/HBoxContainer2/Cool
+func generate_button_name(theme : Theme) -> String:
+	var string = ""
+	var normal_style : StyleBoxFlat = theme.get_stylebox("normal", "Button")
+	
+	if (normal_style.skew.x > 0):
+		string += "skewed"
+	else:
+		string += "straight"
+	if (normal_style.shadow_size == 0):
+		string += "Shadowless"
+	else:
+		string += "Shadow"
+		
+	if ((normal_style.border_width_bottom + normal_style.border_width_left
+		+ normal_style.border_width_top + normal_style.border_width_right) == 0):
+		string += "Borderless"
+	else:
+		string += "Bordered"
+		string += get_color_from_rgb(normal_style.border_color)
+		
+	if (normal_style.border_width_top > 0):
+		string += "Top"
+	if (normal_style.border_width_bottom > 0):
+		string += "Bottom"
+	if (normal_style.border_width_left > 0):
+		string += "Left"
+	if (normal_style.border_width_right > 0):
+		string += "Right"
+	
+	string += get_color_from_rgb(normal_style.bg_color)
+	return shorten_string(string)
+	
+func get_color_from_rgb(color : Color) -> String:
+	if (color[0] > color[1] + color[2]):
+		return "Red"
+	if (color[1] > color[0] + color[2]):
+		return "Green"
+	if (color[2] > color[0] + color[1]):
+		return "Blue"
+	if (color[0] + color[1] > 1):
+		return "Yellow"
+	if (color[0] + color[2] > 1):
+		return "Magenta"
+	if (color[1] + color[2] > 1):
+		return "Cyan"
+	return ""
+	
+func shorten_string(str : String) -> String:
+	str = str.replace("a", "")
+	str = str.replace("e", "")
+	str = str.replace("i", "")
+	str = str.replace("o", "")
+	str = str.replace("u", "")
+	return str
 
 func _on_warm_value_changed(value: float) -> void:
 	cool.value = 1.0 - value
 	update_values()
 
-
 func _on_cool_value_changed(value: float) -> void:
 	warm.value = 1.0 - value
 	update_values()
+	
+func _on_sharp_value_changed(value: float) -> void:
+	smooth.value = 1.0 - value
+	update_values()
 
-func update_values() -> void:
-	warmness = warm.value
-	coolness = cool.value
-
+func _on_round_value_changed(value: float) -> void:
+	sharp.value = 1.0 - value
+	update_values()
 
 func _on_skew_toggled(toggled_on: bool) -> void:
 	skewed = toggled_on
+	
